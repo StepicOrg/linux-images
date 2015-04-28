8aa13b704a3da97678bf994615ad94e94925fb79
Pavel Sviderski <pavel@psviderski.name>
2015-04-28 17:53:49 +0300

    init challenge-ubuntu image

---
+++ challenge-ubuntu/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+service ssh start > /dev/null || exit 1
+
+echo "Press ENTER to start hacking..."
+trap '' 2  # mock SIGINT on CTRL+C
+read
+trap 2  # enable SIGINT
+
+cat /.box/.greeting 2> /dev/null
+while true; do
+    su box
+done
