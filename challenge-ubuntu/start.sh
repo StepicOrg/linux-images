ec3fe84b0cbd6f87fb06412d82d55e5366c307ee
Anton Ovchinnikov <revolver112@gmail.com>
2015-04-29 23:13:38 +0200

    Avoid a new line after the prompt message, add comments

--- challenge-ubuntu/start.sh
+++ challenge-ubuntu/start.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# Start the SSH service and exit on error
 service ssh start > /dev/null || exit 1
 
-echo "Press ENTER to start hacking..."
+# Display the bold prompt
+bold=$(tput bold)
+normal=$(tput sgr0)
+echo -n "${bold}Press ENTER to start hacking...${normal}"
+
+# Wait for ENTER
 trap '' 2  # mock SIGINT on CTRL+C
 read
 trap 2  # enable SIGINT
 
+# Print the greeting
 cat /.box/greeting 2> /dev/null
+
 while true; do
     su box
 done
