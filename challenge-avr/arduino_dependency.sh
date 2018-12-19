ebb6770a504e9b3efaf421b47e669587a9c7cdd9
Konstantin Chayka <pro100kot@users.noreply.github.com>
2018-12-19 15:10:07 +0300

    Challenge avr: arduino ide and arduino-makefile install (#16)

---
+++ challenge-avr/arduino_dependency.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ard_ide_v="1.8.8"
+
+# Base makefile
+git clone https://github.com/sudar/Arduino-Makefile /root/Arduino-Makefile
+chmod -R o+rx /root/Arduino-Makefile
+# Download and unpack Arduino IDE
+wget --no-verbose https://downloads.arduino.cc/arduino-${ard_ide_v}-linux64.tar.xz -P /root
+tar xf /root/arduino-${ard_ide_v}-linux64.tar.xz --directory /root
+mv /root/arduino-${ard_ide_v} /root/arduino-ide
+chmod -R o+rx /root/arduino-ide
+rm -rf /root/arduino-${ard_ide_v}-linux64.tar.xz
